5984445f6fd44a8ba774cb25ef73d79b35c159a3
Stanislau Haidukou <haidukou.stanislau.github@gmail.com>
2023-03-28 19:49:17 +0300

    first commit

---
+++ Lab2/OOP_Passenger_Operations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Passenger_Operations
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
